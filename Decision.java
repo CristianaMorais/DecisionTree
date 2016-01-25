54d32bddd9737c272b548ae01b8d93e50c82ceea
berna46 <bernardo.lfccosta@gmail.com>
2016-01-25 20:06:01 +0000

    Update Decision.java

--- Decision.java
+++ Decision.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 
-public class Arvoredec extends Node{
+public class Decision extends Node{
 	
     static int classIndex,nLines;
     //representa a àrvore de decisão. O 1º campo diz respeito ao pai do nó 'Node'
